d0f7377e84979f1022e4e102925e9e584be7e4e8
BaltimoreOriole <trechenomalleyworks.inc@gmail.com>
2025-10-28 14:07:13 +0000

    h

---
+++ Bin.java
@@ -0,0 +1,32 @@
+package com.binary
+import java.util.Scanner;
+public class Bin {
+    public void binTrans() {
+        int egg = 1;
+        while (egg = 1) {
+            System.out.println("Type N for number to binary, type B for binary to number: ")
+                String that = input.nextLine();
+                if (that == "N") {
+                    System.out.println("Enter number to convert to B")
+
+            }
+            
+                
+
+
+            }
+            if (that == "B") {
+
+            }
+            else {
+                System.out.println("Please enter one of the above letters.")
+                egg = 1
+
+        }
+        
+        }
+        
+
+    }
+
+}
