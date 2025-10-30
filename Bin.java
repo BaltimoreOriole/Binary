811c9c6e98317d34abe1ef1f89efd6fe724b3e05
BaltimoreOriole <trechenomalleyworks.inc@gmail.com>
2025-10-30 12:36:56 +0000

    apopgns

--- Bin.java
+++ Bin.java
@@ -1,15 +1,43 @@
-package com.binary;
 import java.util.Scanner;
 public class Bin {
     public void binTrans() {
+        Scanner scanner = new Scanner (System.in);
         int egg = 1;
         while (egg == 1) {
-            System.out.printLn("Please enter a whole number for Binary conversion: ");
+            System.out.println("Please enter a whole number for Binary conversion: ");
             int num = scanner.nextInt();
-            while 
-            String numWord = "";
-            binNum = num;
-            binNumRe = binNum % 2;
+            int b = 1;
+            int binNumFin = 0;
+            int binTen = 1;
+            while (b == 1) {
+                int binNum = num;
+                int binNumRem = binNum % 2;
+
+                if (binNumRem == 1) {
+                    binNum = binNum - 1;   
+                }
+
+                binNum = binNum / 2;
+
+                
+
+                int binNumCom = binNumRe * binTen;
+                binNumFin = binNumFin + binNumCom;
+                binTen = binTen * 10;
+
+                if (binNum == 1) {
+                    binNumRem = 1;
+                    b = 2;
+                }
+            }
+            
+
+
+
+                
+
+            }
+
 
         }
     }
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-package com.binary;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner jeff = new Scanner(System.in);
+        Bin jeff = new Bin();
+        jeff.binTrans(); 
         
 
     }
