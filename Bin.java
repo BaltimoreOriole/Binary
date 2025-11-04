4251dff7b00f286978baa63291567ddde3416768
BaltimoreOriole <trechenomalleyworks.inc@gmail.com>
2025-11-04 02:03:50 +0000

    d

--- Bin.java
+++ Bin.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
 public class Bin {
-    public void binTrans() {
-        Scanner scanner = new Scanner (System.in);
-        int egg = 1;
-        while (egg == 1) {
-            System.out.println("Please enter a whole number for Binary conversion: ");
-            int num = scanner.nextInt();
-            int b = 1;
-            int binNumFin = 0;
-            int binTen = 1;
-            while (b == 1) {
-                int binNum = num;
-                int binNumRem = binNum % 2;
+    public static void main(String[] args) { 
+        public binTrans() {
+            Scanner scanner = new Scanner (System.in);
+            int egg = 1;  
+            
+            while (egg == 1) {
+                System.out.println("Please enter a whole number for Binary conversion: ");
+                int num = scanner.nextInt();
+                int b = 1;
+                int binNumFin = 0;
+                int binTen = 1;
+                while (b == 1) {
+                    int binNum = num;
+                    int binNumRem = binNum % 2;
 
-                if (binNumRem == 1) {
-                    binNum = binNum - 1;   
-                }
+                    if (binNumRem == 1) {
+                        binNum = binNum - 1;   
+                    }
 
-                binNum = binNum / 2;
+                    binNum = binNum / 2;
 
                 
 
-                int binNumCom = binNumRe * binTen;
-                binNumFin = binNumFin + binNumCom;
-                binTen = binTen * 10;
+                    int binNumCom = binNumRem * binTen;
+                    binNumFin = binNumFin + binNumCom;
+                    binTen = binTen * 10;
 
-                if (binNum == 1) {
-                    binNumRem = 1;
-                    b = 2;
+                    if (binNum == 1) {
+                        binNumRem = 1;
+                        b = 2;
+                    }
                 }
+                System.out.println(num + " in binary is " + binNumFin);
             }
-            System.out.println(num + " in binary is " + binNumFin);
-            
+        }    
 
 
-        }
+        
 
 
     }
